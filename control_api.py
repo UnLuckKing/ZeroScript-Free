cbfc9fb9274835a77429cf1c1cf882f6006bb3ac
UnLuckKing <127764080+UnLuckKing@users.noreply.github.com>
2026-07-12 22:38:12 +0300

    release: sync control API version 1.29.0

--- control_api.py
+++ control_api.py
@@ -14,7 +14,7 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from pathlib import Path
 from typing import Any
 
-VERSION = "1.28.0"
+VERSION = "1.29.0"
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = 17614
 ALLOWED_ACTIONS = {
