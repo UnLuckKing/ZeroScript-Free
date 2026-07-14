1649ac34cc688693ba6e92cee5a23f5f628bee5b
UnLuckKing <127764080+UnLuckKing@users.noreply.github.com>
2026-07-14 12:52:10 +0300

    release: sync Hub control API version 1.32.1

--- control_api.py
+++ control_api.py
@@ -14,7 +14,7 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from pathlib import Path
 from typing import Any
 
-VERSION = "1.32.0"
+VERSION = "1.32.1"
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = 17614
 ALLOWED_ACTIONS = {
